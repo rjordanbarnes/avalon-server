d72f07acdc7aba43fb823f8cada172fe8a3cce65
Jordan Barnes <rjbarnes@uw.edu>
2020-10-06 01:01:14 -0700

    Finish starting game logic

--- Model/Game.cs
+++ Model/Game.cs
@@ -1,21 +1,27 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Avalon.Server.Model
 {
     public class Game
     {
+        private static readonly int MIN_PLAYERS = 5;
         private static readonly int MAX_PLAYERS = 10;
+        private Random random = new Random();
 
         public string gameId { get; }
-        public Player host { get { return players[0]; } set { players[0] = value; } }
+        public Player host { get; private set; }
+        public Player leader { get; private set; }
         public List<Player> players { get; }
-        public GamePhase gamePhase { get; }
+        public List<Team> questResults { get; }
+        public GamePhase gamePhase { get; private set; }
 
         public Game(Player host)
         {
             this.gameId = "TEST";
             this.host = host;
+            this.players.Add(host);
             this.gamePhase = GamePhase.Lobby;
         }
 
@@ -24,6 +30,7 @@ namespace Avalon.Server.Model
             if (!gamePhase.Equals(GamePhase.Lobby))
             {
                 // Not in lobby.
+                return;
             }
 
             if (players.Find(player => player.connectionId.Equals(connectionId)) != null)
@@ -52,6 +59,7 @@ namespace Avalon.Server.Model
             if (!gamePhase.Equals(GamePhase.Lobby))
             {
                 // Not in lobby.
+                return;
             }
 
             if (host.connectionId.Equals(connectionId))
@@ -67,9 +75,41 @@ namespace Avalon.Server.Model
             if (!gamePhase.Equals(GamePhase.Lobby))
             {
                 // Not in lobby.
+                return;
             }
 
-            throw new NotImplementedException();
+            if (players.Count < MIN_PLAYERS)
+            {
+                // Not enough players.
+                return;
+            }
+
+            // Assign random roles
+            List<Role> roles = new List<Role>();
+            int numEvil = (int)Math.Ceiling(this.players.Count / 3.0);
+            int numGood = this.players.Count - numEvil;
+
+            for (int i = 0; i < numEvil; i++)
+            {
+                roles.Add(Role.MinionOfMordred);
+            }
+
+            for (int i = 0; i < numGood; i++)
+            {
+                roles.Add(Role.ServantOfArthur);
+            }
+
+            roles = roles.OrderBy(a => random.Next()).ToList();
+
+            for (int i = 0; i < players.Count; i++)
+            {
+                players[i].role = roles[i];
+            }
+
+            // Assign random leader
+            this.leader = this.players[random.Next(players.Count)];
+
+            this.gamePhase = GamePhase.PartySelection;
         }
 
         public bool ContainsConnection(string connectionId)
